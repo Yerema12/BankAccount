721a9bf95a9a6ab9fa3a49ea12494fef3fbdcde5
Yerema12 <tusekenov.ermek@mail.ru>
2026-01-02 22:58:25 +0500

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,44 +2,35 @@ public class BankAccount {
     private String accountNumber;
     private double balance;
 
-    public BankAccount(String accnum, double balik) {
-        accountNumber = accnum;
-        balance = balik;
+    public BankAccount(String accNum, double bal) {
+        accountNumber = accNum;
+        balance = bal;
     }
 
-    public String getAccountNumber() {
-        return accountNumber;
-    }
+    public String getAccountNumber() { return accountNumber; }
+    public double getBalance() { return balance; }
 
-    public void setAccountNumber(String accnum) {
-        accountNumber = accnum;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balik) {
-        balance = balik;
-    }
-
-    public void deposit(double amount) {
-        balance += amount;
-    }
+    public void deposit(double amount) { balance += amount; }
 
     public void withdraw(double amount) {
-        if (amount <= balance) {
-            balance -= amount;
-        } else {
-            System.out.println("Insufficient funds");
-        }
+        if (amount <= balance) balance -= amount;
+        else System.out.println("Insufficient funds");
     }
 
     @Override
     public String toString() {
         return "Account Number: " + accountNumber + ", Balance: " + balance;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof BankAccount)) return false;
+        BankAccount other = (BankAccount) obj;
+        return accountNumber.equals(other.accountNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return accountNumber.hashCode();
+    }
 }
-
-
-
