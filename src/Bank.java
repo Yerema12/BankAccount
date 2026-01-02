ce6ae7d6b041a423cfe0c4cd05ed724a632e7296
Yerema12 <tusekenov.ermek@mail.ru>
2026-01-02 22:57:59 +0500

    Update Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -1,16 +1,32 @@
+import java.util.*;
+
 public class Bank {
     private String bankName;
+    private List<Customer> customers = new ArrayList<>();
 
-    public Bank(String bankAty) {
-        bankName = bankAty;
+    public Bank(String n) { bankName = n; }
+
+    public void addCustomer(Customer c) { customers.add(c); }
+
+    public void showCustomers() {
+        for (Customer c : customers)
+            System.out.println(bankName + " - " + c);
     }
 
-    public String getBankName() {
-        return bankName;
+    public void sortByBalance() {
+        customers.sort(Comparator.comparingDouble(c -> c.getAccount().getBalance()));
     }
 
-    public void showCustomer(Customer customer) {
-        System.out.println(bankName + " - " + customer);
+    public Customer findCustomerByName(String n) {
+        for (Customer c : customers)
+            if (c.getName().equalsIgnoreCase(n)) return c;
+        return null;
+    }
+
+    public List<VIPCustomer> getVIPCustomers() {
+        List<VIPCustomer> vipList = new ArrayList<>();
+        for (Customer c : customers)
+            if (c instanceof VIPCustomer) vipList.add((VIPCustomer) c);
+        return vipList;
     }
 }
-
