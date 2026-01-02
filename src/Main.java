30a8af8c4fd3693ebbd538e3f96b13fb94435b6b
Yerema12 <tusekenov.ermek@mail.ru>
2026-01-02 22:58:50 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,54 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount("KZ001", 1000);
-        BankAccount account2 = new BankAccount("KZ002", 500);
+        Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank("Kaspi Bank");
 
-        Customer customer1 = new Customer("Ayan", account1);
-        Customer customer2 = new Customer("Dias", account2);
+        System.out.println("Enter number of customers:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-        Bank bank = new Bank("Kaspi bank");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter customer name:");
+            String name = scanner.nextLine();
 
-        bank.showCustomer(customer1);
-        bank.showCustomer(customer2);
+            System.out.println("Enter account number:");
+            String accNum = scanner.nextLine();
 
-        System.out.println("Comparing balances:");
-        if (account1.getBalance() > account2.getBalance()) {
-            System.out.println(customer1.getName() + " has more money");
-        } else {
-            System.out.println(customer2.getName() + " has more money");
+            System.out.println("Enter balance:");
+            double balance = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("Is this customer VIP? (yes/no):");
+            String isVip = scanner.nextLine();
+
+            if (isVip.equalsIgnoreCase("yes")) {
+                System.out.println("Enter VIP bonus rate (in %):");
+                double bonus = scanner.nextDouble();
+                scanner.nextLine();
+                VIPCustomer vip = new VIPCustomer(name, new BankAccount(accNum, balance), bonus);
+                bank.addCustomer(vip);
+            } else {
+                Customer customer = new Customer(name, new BankAccount(accNum, balance));
+                bank.addCustomer(customer);
+            }
         }
+
+        System.out.println("\nAll customers:");
+        bank.showCustomers();
+
+        System.out.println("\nSorting customers by balance:");
+        bank.sortByBalance();
+        bank.showCustomers();
+
+        System.out.println("\nEnter a name to search:");
+        String searchName = scanner.nextLine();
+        Customer found = bank.findCustomerByName(searchName);
+        if (found != null) System.out.println("Found: " + found);
+        else System.out.println("Customer not found");
+
+        System.out.println("\nVIP Customers:");
+        for (VIPCustomer vip : bank.getVIPCustomers()) System.out.println(vip);
     }
 }
