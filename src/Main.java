21ece10e830d1ca2d0e113d84494d1fee41b0909
Yerema12 <tusekenov.ermek@mail.ru>
2026-01-17 02:46:21 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,9 +33,50 @@ public class Main {
                 scanner.nextLine();
                 VIPCustomer vip = new VIPCustomer(name, new BankAccount(accNum, balance), bonus);
                 bank.addCustomer(vip);
+                try (Connection conn = DBConnection.getConnection()) {
+                    PreparedStatement ps1 = conn.prepareStatement(
+                            "INSERT INTO bank_account VALUES (?, ?)"
+                    );
+                    ps1.setString(1, accNum);
+                    ps1.setDouble(2, balance);
+                    ps1.executeUpdate();
+
+                    PreparedStatement ps2 = conn.prepareStatement(
+                            "INSERT INTO customer (name, account_number, is_vip, bonus_rate) VALUES (?, ?, ?, ?)"
+                    );
+                    ps2.setString(1, name);
+                    ps2.setString(2, accNum);
+                    ps2.setBoolean(3, true);
+                    ps2.setDouble(4, bonus);
+                    ps2.executeUpdate();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             } else {
                 Customer customer = new Customer(name, new BankAccount(accNum, balance));
                 bank.addCustomer(customer);
+                try (Connection conn = DBConnection.getConnection()) {
+                    PreparedStatement ps1 = conn.prepareStatement(
+                            "INSERT INTO bank_account VALUES (?, ?)"
+                    );
+                    ps1.setString(1, accNum);
+                    ps1.setDouble(2, balance);
+                    ps1.executeUpdate();
+
+                    PreparedStatement ps2 = conn.prepareStatement(
+                            "INSERT INTO customer (name, account_number, is_vip, bonus_rate) VALUES (?, ?, ?, ?)"
+                    );
+                    ps2.setString(1, name);
+                    ps2.setString(2, accNum);
+                    ps2.setBoolean(3, false);
+                    ps2.setDouble(4, 0);
+                    ps2.executeUpdate();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -52,3 +97,4 @@ public class Main {
         for (VIPCustomer vip : bank.getVIPCustomers()) System.out.println(vip);
     }
 }
+
